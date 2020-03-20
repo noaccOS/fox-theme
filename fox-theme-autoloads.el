a962735130570a9aaf0c596eb654acab1d341702
Francesco Noacco <francesco.noacco2000@gmail.com>
2020-03-20 12:38:08 +0100

    initial release

---
+++ fox-theme-autoloads.el
@@ -0,0 +1,11 @@
+;;
+
+;;;### (autoloads nil "fox-theme" "fox-theme.el" (24179 31291 761306
+;;;;;;  631000))
+;;; Generated autoloads from fox-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "fox-theme" '(#("true-color-p" 0 12 (face font-lock-function-name-face fontified nil)) #("fox-theme-" 0 10 (fontified nil face font-lock-variable-name-face)))))
+
+;;;***
---
+++ fox-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "fox-theme" "0.1" "Fox theme for noaccOS' rice" 'nil :url "https://github.com/noaccOS/fox-theme" :keywords '("color" "theme"))
